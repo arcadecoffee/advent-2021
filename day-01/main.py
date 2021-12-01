fce6bdc565151ef3489e470c90c317bc1eab7c2d
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-01 16:06:55 -0600

    Day 1 - consolidated

--- day-01/main.py
+++ day-01/main.py
@@ -4,54 +4,30 @@ https://adventofcode.com/2021/day/1
 """
 
 
-def part1(infile: str) -> int:
+def count_increases(infile_path: str, window_size: int) -> int:
     """
-    Day 1, Part 1
-    :param infile: file path to input file with integer values separated by newlines
-    :type infile: str
+    Calculate instances of value increases over windows of the specified size
+    :param infile_path: file path to input file with integer values separated by newlines
+    :type infile_path: str
+    :param window_size: number of values to use when looking for increases during the input series
+    :type window_size: int
     :return: count of 'increases' in values in the input file
     :rtype: int
     """
     increases = 0
-
-    with open(infile, 'r', encoding='ascii') as data:
-        prev = int(next(data))
-        for curr in data:
-            curr = int(curr)
-            increases += int(curr > prev)
-            prev = curr
-
-    return increases
-
-
-def part2(infile: str) -> int:
-    """
-    Day 1, Part 2
-    :param infile: file path to input file with integer values separated by newlines
-    :type infile: str
-    :return: count of 'increases' in values in the input file
-    :rtype: int
-    """
-    increases = 0
-
-    with open(infile, 'r', encoding='ascii') as data:
-        prev_set = [int(next(data)) for _ in range(3)]
+    with open(infile_path, 'r', encoding='ascii') as data:
+        prev_set = [int(next(data)) for _ in range(window_size)]
         for val in data:
             curr_set = prev_set[1:] + [int(val)]
             increases += int(sum(curr_set) > sum(prev_set))
             prev_set = curr_set
-
     return increases
 
 
-def main() -> None:
-    """
-    Invoke parts 1 & 2
-    """
-    infile = 'input.txt'
-    print(f'Part 1: {part1(infile)}')
-    print(f'Part 2: {part2(infile)}')
-
-
 if __name__ == '__main__':
-    main()
+    INPUT_FILE = 'input.txt'
+    part1_answer = count_increases(INPUT_FILE, 1)
+    print(f'Part 1: {part1_answer}')
+
+    part2_answer = count_increases(INPUT_FILE, 3)
+    print(f'Part 2: {part2_answer}')
