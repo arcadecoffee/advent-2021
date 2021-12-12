999e30f5e452ac1630d47a6496d5723a45ebddd1
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-12 13:57:26 -0600

    day 13 - empty

---
+++ tests/test_day13.py
@@ -0,0 +1,38 @@
+"""
+Tests for Day 13
+"""
+
+from day13.module import part_1, part_2, \
+    FULL_INPUT_FILE, TEST_INPUT_FILE
+
+
+def test_part_1():
+    """
+    Result should be: -1
+    """
+    result = part_1(TEST_INPUT_FILE)
+    assert result == -1
+
+
+def test_part_1_full():
+    """
+    Result should be: -1
+    """
+    result = part_1(FULL_INPUT_FILE)
+    assert result == -1
+
+
+def test_part_2():
+    """
+    Result should be: -1
+    """
+    result = part_2(TEST_INPUT_FILE)
+    assert result == -1
+
+
+def test_part_2_full():
+    """
+    Result should be: -1
+    """
+    result = part_2(FULL_INPUT_FILE)
+    assert result == -1
