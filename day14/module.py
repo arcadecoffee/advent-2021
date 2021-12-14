4314dc143b3dc8150309bf982b00087094593ec6
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-14 09:21:24 -0600

    day 14 - part 1

--- day14/module.py
+++ day14/module.py
@@ -3,7 +3,8 @@ Advent of Code 2021 - Day 14
 https://adventofcode.com/2021/day/14
 """
 
-from typing import List
+from collections import Counter
+from typing import List, Tuple, Dict, Any
 
 DAY = 14
 
@@ -11,17 +12,35 @@ TEST_INPUT_FILE = f'../inputs/day{DAY:02d}/input.test.txt'
 FULL_INPUT_FILE = f'../inputs/day{DAY:02d}/input.full.txt'
 
 
-def load_data(infile_path: str) -> List[str]:
-    data = []
+def load_data(infile_path: str) -> Tuple[str, Dict[str, str]]:
     with open(infile_path, 'r', encoding='ascii') as infile:
+        template = infile.readline().strip()
+        infile.readline()
+        rules = {}
         for line in infile:
-            data.append(line.strip())
-    return line
+            key, value = line.strip().split(' -> ')
+            rules[key] = value
+    return template, rules
+
+
+def pair_expander(left: str, right: str, rules: Dict[str, str], steps: int):
+    if steps == 0:
+        yield right
+    else:
+        yield from pair_expander(left, rules[left + right], rules, steps - 1)
+        yield from pair_expander(rules[left + right], right, rules, steps - 1)
+
+
+def template_expander(infile_path: str, steps: int):
+    template, rules = load_data(infile_path)
+    yield template[0]
+    for i in range(1, len(template)):
+        yield from pair_expander(template[i - 1], template[i], rules, steps)
 
 
 def part_1(infile_path: str) -> int:
-    data = load_data(infile_path)
-    return -1
+    counts = Counter(template_expander(infile_path, 10))
+    return max(counts.values()) - min(counts.values())
 
 
 def part_2(infile_path: str) -> int:
--- tests/test_day14.py
+++ tests/test_day14.py
@@ -2,24 +2,45 @@
 Tests for Day 14
 """
 
-from day14.module import part_1, part_2, \
+from collections import Counter
+
+from day14.module import part_1, part_2, template_expander, \
     FULL_INPUT_FILE, TEST_INPUT_FILE
+
+
+def test_template_expander():
+    assert ''.join(template_expander(TEST_INPUT_FILE, 0)) == 'NNCB'
+    assert ''.join(template_expander(TEST_INPUT_FILE, 1)) == 'NCNBCHB'
+    assert ''.join(template_expander(TEST_INPUT_FILE, 2)) == 'NBCCNBBBCBHCB'
+    assert ''.join(template_expander(TEST_INPUT_FILE, 3)) == 'NBBBCNCCNBBNBNBBCHBHHBCHB'
+    assert ''.join(template_expander(TEST_INPUT_FILE, 4)) == \
+           'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB'
+    assert len(''.join(template_expander(TEST_INPUT_FILE, 5))) == 97
+    assert len(''.join(template_expander(TEST_INPUT_FILE, 10))) == 3073
+
+
+def test_template_expander_counts():
+    c = Counter(template_expander(TEST_INPUT_FILE, 10))
+    assert c['B'] == 1749
+    assert c['C'] == 298
+    assert c['H'] == 161
+    assert c['N'] == 865
 
 
 def test_part_1():
     """
-    Result should be: 0
+    Result should be: 1588
     """
     result = part_1(TEST_INPUT_FILE)
-    assert result == 0
+    assert result == 1588
 
 
 def test_part_1_full():
     """
-    Result should be: 0
+    Result should be: 2891
     """
     result = part_1(FULL_INPUT_FILE)
-    assert result == 0
+    assert result == 2891
 
 
 def test_part_2():
