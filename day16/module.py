793e2bd9e71f8038f7d3698c92b34151fa3591dd
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-16 16:24:28 -0600

    day 16 - part 1

--- day16/module.py
+++ day16/module.py
@@ -3,25 +3,85 @@ Advent of Code 2021 - Day 16
 https://adventofcode.com/2021/day/16
 """
 
-from typing import List
+from enum import Enum
+from typing import List, Tuple
 
 DAY = 16
 
-TEST_INPUT_FILE = f'../inputs/day{DAY:02d}/input.test.txt'
 FULL_INPUT_FILE = f'../inputs/day{DAY:02d}/input.full.txt'
 
 
-def load_data(infile_path: str) -> List[List[int]]:
-    data = []
+def load_data(infile_path: str) -> str:
     with open(infile_path, 'r', encoding='ascii') as infile:
-        for line in infile:
-            data.append([int(i) for i in line.strip()])
-    return data
+        return infile.readline().strip()
+
+class Packet:
+    def __init__(self, bits: str):
+        self.bits = bits
+        self.version = int(self.bits[0:3], 2)
+        self.type = int(self.bits[3:6], 2)
+        self.subpackets = []
+        self.size = 6
+
+        if self.type == 4:
+            self.value, literal_length = self._parse_literal(bits[6:])
+            self.size += literal_length
+        else:
+            self.length_type = int(self.bits[6:7], 2)
+            self.size += 1
+
+            if self.length_type == 0:
+                field_size = 15
+                subpacket_length = int(self.bits[7:7 + field_size], 2)
+                self.size = self.size + field_size + subpacket_length
+                self.subpackets = self._parse_all_subpackets(
+                    bits[7 + field_size:7 + field_size + subpacket_length]
+                )
+            else:
+                field_size = 11
+                subpacket_count = int(self.bits[7:7 + field_size], 2)
+                self.size = self.size + field_size
+                for _ in range(subpacket_count):
+                    self.subpackets.append(Packet(bits[self.size:]))
+                    self.size += self.subpackets[-1].size
+
+    @classmethod
+    def _parse_all_subpackets(cls, bits: str) -> List[any]:
+        packets = []
+        while bits:
+            packets.append(Packet(bits))
+            bits = bits[packets[-1].size:]
+        return packets
+
+    @property
+    def version_sum(self) -> int:
+        subpacket_version_sum = 0
+        for subpacket in self.subpackets:
+            subpacket_version_sum += subpacket.version_sum
+        return self.version + subpacket_version_sum
+
+    @classmethod
+    def _parse_literal(cls, bits: str) -> Tuple[int, int]:
+        keep_going = True
+        value_bits = ''
+        length = 0
+        while keep_going:
+            keep_going = int(bits[0])
+            value_bits += bits[1:5]
+            bits = bits[5:]
+            length += 5
+        return int(value_bits, 2), length
+
+
+def calculate_version_sums(data: str) -> int:
+    bits = bin(int(data, 16))[2:].zfill(len(data) * 4)
+    packet = Packet(bits)
+    return packet.version_sum
 
 
 def part_1(infile_path: str) -> int:
     data = load_data(infile_path)
-    return -1
+    return calculate_version_sums(data)
 
 
 def part_2(infile_path: str) -> int:
--- tests/test_day16.py
+++ tests/test_day16.py
@@ -2,25 +2,71 @@
 Tests for Day 16
 """
 
-from day15.module import part_1, part_2, \
-    FULL_INPUT_FILE, TEST_INPUT_FILE
+from day16.module import part_1, part_2, calculate_version_sums, \
+    FULL_INPUT_FILE
+
+TESTS = [
+    {
+        'string': 'D2FE28',
+        'version_sum': int('110', 2)
+    },
+    {
+        'string': '38006F45291200',
+        'version_sum': 1 + int('110', 2) + int('010', 2)
+    },
+    {
+        'string': 'EE00D40C823060',
+        'version_sum': 7 + int('010', 2) + int('100', 2) + int('001', 2)
+    },
+    # {
+    #     'string': '8A004A801A8002F478',
+    #     'version_sum': 16
+    # },
+    # {
+    #     'string': '620080001611562C8802118E34',
+    #     'version_sum': 12
+    # },
+    # {
+    #     'string': 'C0015000016115A2E0802F182340',
+    #     'version_sum': 23
+    # },
+    # {
+    #     'string': 'A0016C880162017C3686B18A3D4780',
+    #     'version_sum': 31
+    # },
+]
 
 
-def test_part_1():
-    result = part_1(TEST_INPUT_FILE)
-    assert result == 0
+def test_part_1_0():
+    test = TESTS[0]
+    assert test['version_sum'] == calculate_version_sums(test['string'])
 
 
-def test_part_1_full():
-    result = part_1(FULL_INPUT_FILE)
-    assert result == 0
+def test_part_1_1():
+    test = TESTS[1]
+    assert test['version_sum'] == calculate_version_sums(test['string'])
 
 
-def test_part_2():
-    result = part_2(TEST_INPUT_FILE)
-    assert result == 0
+def test_part_1_2():
+    test = TESTS[2]
+    assert test['version_sum'] == calculate_version_sums(test['string'])
 
 
-def test_part_2_full():
-    result = part_2(FULL_INPUT_FILE)
-    assert result == 0
+def test_part_1_all():
+    for test in TESTS:
+        assert test['version_sum'] == calculate_version_sums(test['string'])
+
+
+# def test_part_1_full():
+#     result = part_1(FULL_INPUT_FILE)
+#     assert result == 0
+#
+#
+# def test_part_2():
+#     result = part_2(TEST_INPUT_FILE)
+#     assert result == 0
+#
+#
+# def test_part_2_full():
+#     result = part_2(FULL_INPUT_FILE)
+#     assert result == 0
