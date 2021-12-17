55143f29301293b4010c9362c513b915cdec49de
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-17 12:50:19 -0600

    day 17 - cleanup

--- day17/module.py
+++ day17/module.py
@@ -20,21 +20,8 @@ def load_data(infile_path: str) -> Tuple[int, int, int, int]:
         return x1, x2, y1, y2
 
 
-def x_min_max(x1: int, x2: int) -> Tuple[int, int]:
-    minimum = ceil(sqrt(min([x1, x2]) * 8 + 1) / 2 - 1 / 2)
-    maximum = max([x1, x2])
-    return minimum, maximum
-
-
-def y_min_max(y1: int, y2: int) -> Tuple[int, int]:
-    minimum = min([y1, y2])
-    maximum = abs(min([y1, y2])) - 1
-    return minimum, maximum
-
-
-def maximum_altitude(y1: int, y2: int) -> int:
-    t = abs(min([y1, y2]))
-    return int(t * (t - 1) / 2)
+def maximum_altitude(y: int) -> int:
+    return int(y * -1 * (y * -1 - 1) / 2)
 
 
 def shot_good(x_velocity: int, y_velocity: int, x1: int, x2: int, y1: int, y2: int) -> bool:
@@ -54,17 +41,18 @@ def count_good_shots(x1: int, x2: int, y1: int, y2: int) -> int:
     x_max = round(x2 / 2) + 1
     y_min = y1
     y_max = y1 * -1
-    good_shots = []
+    arcing_good_shots = []
     for x in range(x_min, x_max):
         for y in range(y_min, y_max):
             if shot_good(x, y, x1, x2, y1, y2):
-                good_shots.append((x, y))
-    return len(good_shots) + ((x2 + 1 - x1) * (y2 + 1 - y1))
+                arcing_good_shots.append((x, y))
+    direct_shot_count = (x2 + 1 - x1) * (y2 + 1 - y1)
+    return len(arcing_good_shots) + direct_shot_count
 
 
 def part_1(infile_path: str) -> int:
     target_area = load_data(infile_path)
-    return maximum_altitude(*target_area[2:])
+    return maximum_altitude(target_area[2])
 
 
 def part_2(infile_path: str) -> int:
