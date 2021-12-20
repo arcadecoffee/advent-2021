21ebf9444b95949cfe7240307d6ef6de4428982b
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-20 11:50:00 -0600

    day 20 - part 2

--- day20/module.py
+++ day20/module.py
@@ -3,25 +3,97 @@ Advent of Code 2021 - Day 20
 https://adventofcode.com/2021/day/20
 """
 
+from typing import List, Tuple
+
 DAY = '20'
 
 FULL_INPUT_FILE = f'../inputs/day{DAY}/input.full.txt'
 TEST_INPUT_FILE = f'../inputs/day{DAY}/input.test.txt'
 
 
-def load_data(infile_path: str) -> str:
+class Image:
+    def __init__(self, algorithm:str, image: List[str]) -> None:
+        self._algorithm_bitmap = [c == '#' for c in algorithm]
+        self._image_bitmap = [[c == '#' for c in r] for r in image]
+        self.background = False
+
+    def add_margin(self, size: int = 1) -> None:
+        for row in self._image_bitmap:
+            row[0:0] = [self.background] * size
+            row[self.width:self.width] = [self.background] * size
+        self._image_bitmap[0:0] = [[self.background] * self.width] * size
+        self._image_bitmap[self.height:self.height] = [[self.background] * self.width] * size
+
+    def crop(self) -> None:
+        while all(c == self.background for c in self._image_bitmap[0]):
+            del self._image_bitmap[0]
+        while all(c == self.background for c in self._image_bitmap[-1]):
+            del self._image_bitmap[-1]
+        while all(c == self.background for c in [r[0] for r in self._image_bitmap]):
+            for s in self._image_bitmap:
+                del s[0]
+        while all(c == self.background for c in [r[-1] for r in self._image_bitmap]):
+            for s in self._image_bitmap:
+                del s[-1]
+
+    def enhance(self, steps: int = 1) -> None:
+        self.add_margin(steps + 1)
+        for _ in range(steps):
+            new_background = self._algorithm_bitmap[
+                int(''.join([str(int(self.background))] * 9), 2)
+            ]
+
+            new_bitmap = []
+            for i in range(self.height):
+                new_bitmap.append([new_background] * self.width)
+
+            for row_num in range(1, self.height - 1):
+                for col_num in range(1, self.width - 1):
+                    n = [self._image_bitmap[row_num + i][col_num - 1:col_num + 2]
+                         for i in (-1, 0, 1)]
+                    n = int(''.join([str(int(bit)) for row in n for bit in row]), 2)
+                    new_bitmap[row_num][col_num] = self._algorithm_bitmap[n]
+            self._image_bitmap = new_bitmap
+            self.background = new_background
+        self.crop()
+
+    @property
+    def width(self) -> int:
+        return len(self._image_bitmap[0])
+
+    @property
+    def height(self) -> int:
+        return len(self._image_bitmap)
+
+    @property
+    def rendered_image(self) -> str:
+        return '\n'.join([''.join(['#' if c else '.' for c in l]) for l in self._image_bitmap])
+
+    @property
+    def pixel_count(self) -> int:
+        return sum([sum(r) for r in self._image_bitmap])
+
+
+def load_image(infile_path: str) -> Image:
     with open(infile_path, 'r', encoding='ascii') as infile:
-        return infile.readline().strip()
+        algorithm = infile.readline().strip()
+        infile.readline()
+        image = []
+        for line in infile:
+            image.append(line.strip())
+        return Image(algorithm, image)
 
 
 def part_1(infile_path: str) -> int:
-    data = load_data(infile_path)
-    return -1
+    img = load_image(infile_path)
+    img.enhance(2)
+    return img.pixel_count
 
 
 def part_2(infile_path: str) -> int:
-    data = load_data(infile_path)
-    return -1
+    img = load_image(infile_path)
+    img.enhance(50)
+    return img.pixel_count
 
 
 if __name__ == '__main__':
--- tests/test_day20.py
+++ tests/test_day20.py
@@ -2,25 +2,124 @@
 Tests for Day 20
 """
 
-from day20.module import part_1, part_2, \
+from day20.module import part_1, part_2, load_image, \
     FULL_INPUT_FILE, TEST_INPUT_FILE
+
+TEST_RESULT_0 = """
+#..#.
+#....
+##..#
+..#..
+..###
+"""
+TEST_RESULT_0 = TEST_RESULT_0[1:-1]
+
+TEST_RESULT_0_1 = """
+.......
+.#..#..
+.#.....
+.##..#.
+...#...
+...###.
+.......
+"""
+TEST_RESULT_0_1 = TEST_RESULT_0_1[1:-1]
+
+TEST_RESULT_0_5 = """
+...............
+...............
+...............
+...............
+...............
+.....#..#......
+.....#.........
+.....##..#.....
+.......#.......
+.......###.....
+...............
+...............
+...............
+...............
+...............
+"""
+TEST_RESULT_0_5 = TEST_RESULT_0_5[1:-1]
+
+TEST_RESULT_1 = """
+.##.##.
+#..#.#.
+##.#..#
+####..#
+.#..##.
+..##..#
+...#.#.
+"""
+TEST_RESULT_1 = TEST_RESULT_1[1:-1]
+
+TEST_RESULT_2 = """
+.......#.
+.#..#.#..
+#.#...###
+#...##.#.
+#.....#.#
+.#.#####.
+..#.#####
+...##.##.
+....###..
+"""
+TEST_RESULT_2 = TEST_RESULT_2[1:-1]
+
+
+def test_load():
+    img = load_image(TEST_INPUT_FILE)
+    assert img.rendered_image == TEST_RESULT_0
+
+
+def test_margin():
+    img = load_image(TEST_INPUT_FILE)
+    img.add_margin(1)
+    assert img.rendered_image == TEST_RESULT_0_1
+
+
+def test_margin_5():
+    img = load_image(TEST_INPUT_FILE)
+    img.add_margin(5)
+    assert img.rendered_image == TEST_RESULT_0_5
+
+
+def test_crop():
+    img = load_image(TEST_INPUT_FILE)
+    img.add_margin(5)
+    img.crop()
+    assert img.rendered_image == TEST_RESULT_0
+
+
+def test_enhance_1():
+    img = load_image(TEST_INPUT_FILE)
+    img.enhance()
+    assert img.rendered_image == TEST_RESULT_1
+
+
+def test_enhance_2():
+    img = load_image(TEST_INPUT_FILE)
+    img.enhance(2)
+    assert img.rendered_image == TEST_RESULT_2
 
 
 def test_part_1():
     result = part_1(TEST_INPUT_FILE)
-    assert result == 0
+    assert result == 35
 
 
 def test_part_1_full():
     result = part_1(FULL_INPUT_FILE)
-    assert result == 0
+    assert result == 5065
 
 
 def test_part_2():
     result = part_2(TEST_INPUT_FILE)
-    assert result == 0
+    assert result == 3351
 
 
 def test_part_2_full():
     result = part_2(FULL_INPUT_FILE)
-    assert result == 0
+    assert result == 14790
