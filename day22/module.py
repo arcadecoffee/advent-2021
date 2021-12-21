0808a672b3bc71d8e4b140f048607c6b9601421c
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-21 16:23:09 -0600

    day 22 - template

---
+++ day22/module.py
@@ -0,0 +1,32 @@
+"""
+Advent of Code 2021 - Day 22
+https://adventofcode.com/2021/day/22
+"""
+
+DAY = '22'
+
+FULL_INPUT_FILE = f'../inputs/day{DAY}/input.full.txt'
+TEST_INPUT_FILE = f'../inputs/day{DAY}/input.test.txt'
+
+
+def load_data(infile_path: str) -> str:
+    with open(infile_path, 'r', encoding='ascii') as infile:
+        return infile.readline().strip()
+
+
+def part_1(infile_path: str) -> int:
+    data = load_data(infile_path)
+    return 0
+
+
+def part_2(infile_path: str) -> int:
+    data = load_data(infile_path)
+    return 0
+
+
+if __name__ == '__main__':
+    part1_answer = part_1(FULL_INPUT_FILE)
+    print(f'Part 1: {part1_answer}')
+
+    part2_answer = part_2(FULL_INPUT_FILE)
+    print(f'Part 2: {part2_answer}')
