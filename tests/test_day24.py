14a03aea1c39cea55256ff9631647f7490882ccd
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-26 00:09:18 -0600

    day 24 - templates

---
+++ tests/test_day24.py
@@ -0,0 +1,26 @@
+"""
+Tests for Day 24
+"""
+
+from day24.module import part_1, part_2, \
+    FULL_INPUT_FILE, TEST_INPUT_FILE
+
+
+def test_part_1():
+    result = part_1(TEST_INPUT_FILE)
+    assert result == 0
+
+
+def test_part_1_full():
+    result = part_1(FULL_INPUT_FILE)
+    assert result == 0
+
+
+def test_part_2():
+    result = part_2(TEST_INPUT_FILE)
+    assert result == 0
+
+
+def test_part_2_full():
+    result = part_2(FULL_INPUT_FILE)
+    assert result == 0
