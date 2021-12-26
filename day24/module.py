d4437747b6c74e6a0f90b8d38967456c8d84f0b4
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-26 15:07:32 -0600

    day 24 - alu and tests

--- day24/module.py
+++ day24/module.py
@@ -3,14 +3,14 @@ Advent of Code 2021 - Day 24
 https://adventofcode.com/2021/day/24
 """
 
-from typing import List
+from typing import Any, List
 
 DAY = '24'
 
 FULL_INPUT_FILE = f'../inputs/day{DAY}/input.full.txt'
 TEST1_INPUT_FILE = f'../inputs/day{DAY}/input.test1.txt'
 TEST2_INPUT_FILE = f'../inputs/day{DAY}/input.test2.txt'
-TEST3_INPUT_FILE = f'../inputs/day{DAY}/input.test3txt'
+TEST3_INPUT_FILE = f'../inputs/day{DAY}/input.test3.txt'
 
 
 def load_data(infile_path: str) -> List[str]:
@@ -18,9 +18,41 @@ def load_data(infile_path: str) -> List[str]:
         return [l.strip() for l in infile.readlines()]
 
 
+class ArithmeticLogicUnitRegister:
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, '_registers')[self.name]
+
+    def __set__(self, obj, value):
+        getattr(obj, '_registers')[self.name] = value
+
+
 class ArithmeticLogicUnit:
+    w = ArithmeticLogicUnitRegister()
+    x = ArithmeticLogicUnitRegister()
+    y = ArithmeticLogicUnitRegister()
+    z = ArithmeticLogicUnitRegister()
+
     def __init__(self, w: int = 0, x: int = 0, y: int = 0, z: int = 0):
         self._registers = {'w': w, 'x': x, 'y': y, 'z': z}
+
+    def execute(self, instructions: List[str], inputs: List[int] = None):
+        operations = {
+            'inp': lambda a, b: inputs.pop(0),
+            'add': lambda a, b: self._registers[a] + b,
+            'mul': lambda a, b: self._registers[a] * b,
+            'div': lambda a, b: int(self._registers[a] / b),
+            'mod': lambda a, b: self._registers[a] % b,
+            'eql': lambda a, b: int(self._registers[a] == b)
+        }
+
+        for instruction in instructions:
+            operation, arg_a, arg_b = (instruction + ' 0').split(' ')[:3]
+            arg_b = self._registers[arg_b] if arg_b.isalpha() else int(arg_b)
+            self._registers[arg_a] = (operations[operation])(arg_a, arg_b)
 
 
 def part_1(infile_path: str) -> int:
@@ -34,6 +66,8 @@ def part_2(infile_path: str) -> int:
 
 
 if __name__ == '__main__':
+    aaaa = ArithmeticLogicUnit()
+    aaaa.execute(['inp x', 'mul x -1'], [42])
     part1_answer = part_1(FULL_INPUT_FILE)
     print(f'Part 1: {part1_answer}')
 
--- tests/test_day24.py
+++ tests/test_day24.py
@@ -2,13 +2,34 @@
 Tests for Day 24
 """
 
-from day24.module import ArithmeticLogicUnit as alu, part_1, part_2, \
+from day24.module import ArithmeticLogicUnit, load_data, part_1, part_2, \
     FULL_INPUT_FILE, TEST1_INPUT_FILE, TEST2_INPUT_FILE, TEST3_INPUT_FILE
 
 
 def test_alu_1():
-    result = alu(TEST1_INPUT_FILE, [42])
-    assert result.x == -42
+    alu = ArithmeticLogicUnit()
+    alu.execute(load_data(TEST1_INPUT_FILE), [42])
+    assert alu.x == -42
+
+
+def test_alu_2():
+    alu = ArithmeticLogicUnit()
+    alu.execute(load_data(TEST2_INPUT_FILE), [2, 6])
+    assert alu.z == 1
+
+    alu.execute(load_data(TEST2_INPUT_FILE), [2, 5])
+    assert alu.z == 0
+
+
+def test_alu_3():
+    alu = ArithmeticLogicUnit()
+    alu.execute(load_data(TEST3_INPUT_FILE), [42])
+    assert alu.w == 1
+    assert alu.x == 0
+    assert alu.y == 1
+    assert alu.z == 0
+
+
 #
 #
 # def test_part_1_full():
