772c7d132c30201950237c2e5019d739de2143d2
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-28 13:56:39 -0600

    day 25 - part 1 - brute force

--- day25/module.py
+++ day25/module.py
@@ -22,11 +22,11 @@ class CucumberMap:
         # move east
         new_map: List = deepcopy(self.map)
         for row_num in range(len(self.map)):
-            for i in range(len(self.map[row_num])):
-                next_i = i + 1 if i + 1 < len(self.map[row_num]) else 0
-                if self.map[row_num][i] == '>' and not self.map[row_num][next_i]:
+            for col_num in range(len(self.map[row_num])):
+                next_i = col_num + 1 if col_num + 1 < len(self.map[row_num]) else 0
+                if self.map[row_num][col_num] == '>' and not self.map[row_num][next_i]:
                     new_map[row_num][next_i] = '>'
-                    new_map[row_num][i] = None
+                    new_map[row_num][col_num] = None
                     moved = True
         self.map = new_map
 
@@ -39,45 +39,29 @@ class CucumberMap:
                     new_map[row_num][col_num] = None
                     new_map[next_row_num][col_num] = 'v'
                     moved = True
-
         self.map = new_map
+
         return moved
 
-    def dump(self):
-        for r in self.map:
-            print(''.join([c if c else '.' for c in r]))
-
-    def move_and_dump(self):
-        self.move_cucumbers()
-        self.dump()
+    def dump(self) -> str:
+        return '\n'.join([''.join([c if c else '.' for c in r]) for r in self.map]) + '\n'
 
 
 def load_data(infile_path: str) -> List[List[Optional[str]]]:
     data = []
     with open(infile_path, 'r', encoding='ascii') as infile:
-        for line in infile:
-            data.append([p if p != '.' else None for p in line.strip()])
+        [data.append([p if p != '.' else None for p in line.strip()]) for line in infile]
     return data
 
 
 def part_1(infile_path: str) -> int:
-    data = load_data(infile_path)
+    cm = CucumberMap(load_data(infile_path))
     i = 1
-    cm = CucumberMap(data)
     while cm.move_cucumbers():
         i += 1
     return i
 
 
-def part_2(infile_path: str) -> int:
-    data = load_data(infile_path)
-    return 0
-
-
 if __name__ == '__main__':
     part1_answer = part_1(TEST_INPUT_FILE)
-#    part1_answer = part_1(FULL_INPUT_FILE)
     print(f'Part 1: {part1_answer}')
-
-#    part2_answer = part_2(FULL_INPUT_FILE)
-#    print(f'Part 2: {part2_answer}')
--- tests/test_day25.py
+++ tests/test_day25.py
@@ -2,7 +2,7 @@
 Tests for Day 25
 """
 
-from day25.module import part_1, part_2, \
+from day25.module import part_1, \
     FULL_INPUT_FILE, TEST_INPUT_FILE
 
 
@@ -14,13 +14,3 @@ def test_part_1():
 def test_part_1_full():
     result = part_1(FULL_INPUT_FILE)
     assert result == 509
-
-
-def test_part_2():
-    result = part_2(TEST_INPUT_FILE)
-    assert result == 0
-
-
-def test_part_2_full():
-    result = part_2(FULL_INPUT_FILE)
-    assert result == 0
